829a13964b2284228411230f574f17594dab8e80
RekGRpth <rekgrpth@gmail.com>
2019-08-20 14:22:34 +0500

    up

--- pg_htmldoc.c
+++ pg_htmldoc.c
@@ -23,6 +23,42 @@ typedef enum {
 
 PG_MODULE_MAGIC;
 
+static void read_fileurl(const char *fileurl, tree_t **document, const char *path) {
+    tree_t *file;
+    FILE *in;
+    const char *realname = file_find(path, fileurl);
+    const char *base = file_directory(fileurl);
+    if (!realname) ereport(ERROR, (errmsg("!realname")));
+    if (!(in = fopen(realname, "rb"))) ereport(ERROR, (errmsg("!in")));
+    if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!file")));
+    htmlSetVariable(file, (uchar *)"_HD_URL", (uchar *)fileurl);
+    htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)file_basename(fileurl));
+    htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)base);
+    htmlReadFile2(file, in, base);
+    if (*document == NULL) *document = file; else {
+        while ((*document)->next != NULL) *document = (*document)->next;
+        (*document)->next = file;
+        file->prev = *document;
+    }
+    fclose(in);
+}
+
+static void read_html(char *html, size_t len, tree_t **document) {
+    tree_t *file;
+    FILE *in;
+    if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!file")));
+    htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)"");
+    htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)".");
+    if (!(in = fmemopen(html, len, "rb"))) ereport(ERROR, (errmsg("!in")));
+    htmlReadFile2(file, in, ".");
+    if (*document == NULL) *document = file; else {
+        while ((*document)->next != NULL) *document = (*document)->next;
+        (*document)->next = file;
+        file->prev = *document;
+    }
+    fclose(in);
+}
+
 static Datum htmldoc(PG_FUNCTION_ARGS, data_type_t data_type, input_type_t input_type, output_type_t output_type) {
     Datum *elemsp;
     bool *nullsp;
@@ -30,140 +66,70 @@ static Datum htmldoc(PG_FUNCTION_ARGS, data_type_t data_type, input_type_t input
     text *pdf;
     char *output_data = NULL;
     size_t output_len = 0;
-    FILE *in, *out;
-    tree_t *document = NULL, *file;
+    FILE *out;
+    tree_t *document = NULL;
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("data is null!")));
     _htmlPPI = 72.0f * _htmlBrowserWidth / (PageWidth - PageLeft - PageRight);
     htmlSetCharSet("utf-8");
-    if (input_type == INPUT_TYPE_FILE) {
-        if (data_type == DATA_TYPE_ARRAY) {
-            if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) ereport(ERROR, (errcode(ERRCODE_ARRAY_ELEMENT_ERROR), errmsg("array_contains_nulls")));
-            deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
-            for (int i = 0; i < nelemsp; i++) {
-                char *name = TextDatumGetCString(elemsp[i]);
-                const char *realname = file_find(Path, name);
-                const char *base = file_directory(name);
-                if (!realname) ereport(ERROR, (errmsg("!realname")));
-                if (!(in = fopen(realname, "rb"))) ereport(ERROR, (errmsg("!in")));
-                if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!file")));
-                htmlSetVariable(file, (uchar *)"_HD_URL", (uchar *)name);
-                htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)file_basename(name));
-                htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)base);
-                htmlReadFile2(file, in, base);
-                if (document == NULL) document = file; else {
-                    while (document->next != NULL) document = document->next;
-                    document->next = file;
-                    file->prev = document;
+    switch (input_type) {
+        case INPUT_TYPE_FILE: switch (data_type) {
+            case DATA_TYPE_TEXT: read_fileurl(TextDatumGetCString(PG_GETARG_DATUM(0)), &document, Path); break;
+            case DATA_TYPE_ARRAY: {
+                if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) ereport(ERROR, (errcode(ERRCODE_ARRAY_ELEMENT_ERROR), errmsg("array_contains_nulls")));
+                deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
+                for (int i = 0; i < nelemsp; i++) read_fileurl(TextDatumGetCString(elemsp[i]), &document, Path);
+            } break;
+        } break;
+        case INPUT_TYPE_HTML: switch (data_type) {
+            case DATA_TYPE_TEXT: {
+                text *html = DatumGetTextP(PG_GETARG_DATUM(0));
+                read_html(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html), &document);
+                pfree(html);
+            } break;
+            case DATA_TYPE_ARRAY: {
+                if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) ereport(ERROR, (errcode(ERRCODE_ARRAY_ELEMENT_ERROR), errmsg("array_contains_nulls")));
+                deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
+                for (int i = 0; i < nelemsp; i++) {
+                    text *html = DatumGetTextP(elemsp[i]);
+                    read_html(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html), &document);
                 }
-                fclose(in);
-                //pfree(name);
-            }
-        } else {
-            char *name = TextDatumGetCString(PG_GETARG_DATUM(0));
-            const char *realname = file_find(Path, name);
-            const char *base = file_directory(name);
-            if (!realname) ereport(ERROR, (errmsg("!realname")));
-            if (!(in = fopen(realname, "rb"))) ereport(ERROR, (errmsg("!in")));
-            if (!(document = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!document")));
-            htmlSetVariable(document, (uchar *)"_HD_URL", (uchar *)name);
-            htmlSetVariable(document, (uchar *)"_HD_FILENAME", (uchar *)file_basename(name));
-            htmlSetVariable(document, (uchar *)"_HD_BASE", (uchar *)base);
-            htmlReadFile2(document, in, base);
-            fclose(in);
-            pfree(name);
-        }
-    } else if (input_type == INPUT_TYPE_HTML) {
-        if (data_type == DATA_TYPE_ARRAY) {
-            if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) ereport(ERROR, (errcode(ERRCODE_ARRAY_ELEMENT_ERROR), errmsg("array_contains_nulls")));
-            deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
-            for (int i = 0; i < nelemsp; i++) {
-                text *html = DatumGetTextP(elemsp[i]);
-                if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!file")));
-                htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)"");
-                htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)".");
-                if (!(in = fmemopen(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html), "rb"))) ereport(ERROR, (errmsg("!in")));
-                htmlReadFile2(file, in, ".");
-                if (document == NULL) document = file; else {
-                    while (document->next != NULL) document = document->next;
-                    document->next = file;
-                    file->prev = document;
-                }
-                fclose(in);
-                //pfree(html);
-            }
-        } else {
-            text *html = DatumGetTextP(PG_GETARG_DATUM(0));
-            if (!(document = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!document")));
-            htmlSetVariable(document, (uchar *)"_HD_FILENAME", (uchar *)"");
-            htmlSetVariable(document, (uchar *)"_HD_BASE", (uchar *)".");
-            if (!(in = fmemopen(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html), "rb"))) ereport(ERROR, (errmsg("!in")));
-            htmlReadFile2(document, in, ".");
-            fclose(in);
-            pfree(html);
-        }
-    } else if (input_type == INPUT_TYPE_URL) {
-        if (data_type == DATA_TYPE_ARRAY) {
-            if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) ereport(ERROR, (errcode(ERRCODE_ARRAY_ELEMENT_ERROR), errmsg("array_contains_nulls")));
-            deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
-            for (int i = 0; i < nelemsp; i++) {
-                char *url = TextDatumGetCString(elemsp[i]);
-                const char *realname = file_find(NULL, url);
-                const char *base = file_directory(url);
-                if (!realname) ereport(ERROR, (errmsg("!realname")));
-                if (!(in = fopen(realname, "rb"))) ereport(ERROR, (errmsg("!in")));
-                if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!file")));
-                htmlSetVariable(file, (uchar *)"_HD_URL", (uchar *)url);
-                htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)file_basename(url));
-                htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)base);
-                htmlReadFile2(file, in, base);
-                if (document == NULL) document = file; else {
-                    while (document->next != NULL) document = document->next;
-                    document->next = file;
-                    file->prev = document;
-                }
-                fclose(in);
-                //pfree(url);
-            }
-        } else {
-            char *url = TextDatumGetCString(PG_GETARG_DATUM(0));
-            const char *realname = file_find(NULL, url);
-            const char *base = file_directory(url);
-            if (!realname) ereport(ERROR, (errmsg("!realname")));
-            if (!(in = fopen(realname, "rb"))) ereport(ERROR, (errmsg("!in")));
-            if (!(document = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errmsg("!document")));
-            htmlSetVariable(document, (uchar *)"_HD_URL", (uchar *)url);
-            htmlSetVariable(document, (uchar *)"_HD_FILENAME", (uchar *)file_basename(url));
-            htmlSetVariable(document, (uchar *)"_HD_BASE", (uchar *)base);
-            htmlReadFile2(document, in, base);
-            fclose(in);
-            pfree(url);
-        }
+            } break;
+        } break;
+        case INPUT_TYPE_URL: switch (data_type) {
+            case DATA_TYPE_TEXT: read_fileurl(TextDatumGetCString(PG_GETARG_DATUM(0)), &document, NULL); break;
+            case DATA_TYPE_ARRAY: {
+                if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) ereport(ERROR, (errcode(ERRCODE_ARRAY_ELEMENT_ERROR), errmsg("array_contains_nulls")));
+                deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
+                for (int i = 0; i < nelemsp; i++) read_fileurl(TextDatumGetCString(elemsp[i]), &document, NULL);
+            } break;
+        } break;
     }
     htmlFixLinks(document, document, 0);
-    if (output_type == OUTPUT_TYPE_PDF) {
-        PSLevel = 0;
-    } else if (output_type == OUTPUT_TYPE_PS) {
-        PSLevel = 3;
+    switch (output_type) {
+        case OUTPUT_TYPE_PDF: PSLevel = 0; break;
+        case OUTPUT_TYPE_PS: PSLevel = 3; break;
     }
-    if (PG_NARGS() == 1) {
-        if (!(out = open_memstream(&output_data, &output_len))) ereport(ERROR, (errmsg("!out")));
-    } else {
-        char *file;
-        if (PG_ARGISNULL(1)) ereport(ERROR, (errmsg("our is null!")));
-        file = TextDatumGetCString(PG_GETARG_DATUM(1));
-        if (!(out = fopen(file, "wb"))) ereport(ERROR, (errmsg("!out")));
-        pfree(file);
+    switch (PG_NARGS()) {
+        case 1: if (!(out = open_memstream(&output_data, &output_len))) ereport(ERROR, (errmsg("!out"))); break;
+        default: {
+            char *file;
+            if (PG_ARGISNULL(1)) ereport(ERROR, (errmsg("our is null!")));
+            file = TextDatumGetCString(PG_GETARG_DATUM(1));
+            if (!(out = fopen(file, "wb"))) ereport(ERROR, (errmsg("!out")));
+            pfree(file);
+        } break;
     }
     pspdf_export_out(document, NULL, out);
     htmlDeleteTree(document);
     file_cleanup();
     image_flush_cache();
-    if (PG_NARGS() == 1) {
-        pdf = cstring_to_text_with_len(output_data, output_len);
-        free(output_data);
-        PG_RETURN_TEXT_P(pdf);
-    } else {
-        PG_RETURN_BOOL(true);
+    switch (PG_NARGS()) {
+        case 1: {
+            pdf = cstring_to_text_with_len(output_data, output_len);
+            free(output_data);
+            PG_RETURN_TEXT_P(pdf);
+        } break;
+        default: PG_RETURN_BOOL(true); break;
     }
 }
 
