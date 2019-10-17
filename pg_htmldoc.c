51e9a174d7d120b69e1a8dade80660062f57f455
RekGRpth <rekgrpth@gmail.com>
2019-10-17 15:17:35 +0500

    up

--- pg_htmldoc.c
+++ pg_htmldoc.c
@@ -73,7 +73,6 @@ static Datum htmldoc(PG_FUNCTION_ARGS, data_type_t data_type, input_type_t input
     FILE *out;
     tree_t *document = NULL;
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("data is null!")));
-    htmlSetCharSet("utf-8");
     switch (input_type) {
         case INPUT_TYPE_FILE: switch (data_type) {
             case DATA_TYPE_TEXT: read_fileurl(TextDatumGetCString(PG_GETARG_DATUM(0)), &document, Path); break;
