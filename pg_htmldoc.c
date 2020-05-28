08a76d827d81b6cf656b48ea1f98e946f2eaa043
RekGRpth <rekgrpth@gmail.com>
2020-05-28 09:14:59 +0500

    up

--- pg_htmldoc.c
+++ pg_htmldoc.c
@@ -39,116 +39,64 @@
 #define N(fmt, ...) ereport(NOTICE, (errmsg(GET_FORMAT(fmt, ##__VA_ARGS__), ##__VA_ARGS__)))
 #define W(fmt, ...) ereport(WARNING, (errmsg(GET_FORMAT(fmt, ##__VA_ARGS__), ##__VA_ARGS__)))
 
-typedef enum {
-    DATA_TYPE_TEXT = 0,
-    DATA_TYPE_ARRAY
-} data_type_t;
-
-typedef enum {
-    INPUT_TYPE_FILE = 0,
-    INPUT_TYPE_HTML,
-    INPUT_TYPE_URL
-} input_type_t;
-
-typedef enum {
-    OUTPUT_TYPE_PDF = 0,
-    OUTPUT_TYPE_PS
-} output_type_t;
-
 PG_MODULE_MAGIC;
 
-static void read_fileurl(const char *fileurl, tree_t **document, const char *path) {
+tree_t *document = NULL;
+
+void _PG_init(void); void _PG_init(void) {
+    if (!_htmlInitialized) htmlSetCharSet("utf-8");
+}
+
+static void read_fileurl(const char *fileurl) {
     tree_t *file;
     FILE *in;
-    const char *realname = file_find(path, fileurl);
+    const char *realname = file_find(Path, fileurl);
     const char *base = file_directory(fileurl);
     _htmlPPI = 72.0f * _htmlBrowserWidth / (PageWidth - PageLeft - PageRight);
     if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) E("!htmlAddTree");
     htmlSetVariable(file, (uchar *)"_HD_URL", (uchar *)fileurl);
     htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)file_basename(fileurl));
     htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)base);
-    if (!realname) E("!file_find(\"%s\", \"%s\")", path, fileurl);
+    if (!realname) E("!file_find(\"%s\", \"%s\")", Path, fileurl);
     if (!(in = fopen(realname, "rb"))) E("!fopen(\"%s\")", realname);
     htmlReadFile2(file, in, base);
     fclose(in);
-    if (*document == NULL) *document = file; else {
-        while ((*document)->next != NULL) *document = (*document)->next;
-        (*document)->next = file;
-        file->prev = *document;
+    if (document == NULL) document = file; else {
+        while (document->next != NULL) document = document->next;
+        document->next = file;
+        file->prev = document;
     }
 }
 
-static void read_html(char *html, size_t len, tree_t **document) {
+static void read_html(char *html, size_t len) {
     tree_t *file;
     FILE *in;
     _htmlPPI = 72.0f * _htmlBrowserWidth / (PageWidth - PageLeft - PageRight);
     if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) E("!htmlAddTree");
-    htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)"");
+    htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)"html");
     htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)".");
     if (!(in = fmemopen(html, len, "rb"))) E("!fmemopen");
     htmlReadFile2(file, in, ".");
     fclose(in);
-    if (*document == NULL) *document = file; else {
-        while ((*document)->next != NULL) *document = (*document)->next;
-        (*document)->next = file;
-        file->prev = *document;
+    if (document == NULL) document = file; else {
+        while (document->next != NULL) document = document->next;
+        document->next = file;
+        file->prev = document;
     }
 }
 
-static Datum htmldoc(PG_FUNCTION_ARGS, data_type_t data_type, input_type_t input_type, output_type_t output_type) {
-    Datum *elemsp;
-    bool *nullsp;
-    int nelemsp;
+static Datum htmldoc(PG_FUNCTION_ARGS) {
     char *output_data = NULL;
     size_t output_len = 0;
     FILE *out;
-    tree_t *document = NULL;
-    if (PG_ARGISNULL(0)) E("data is null!");
-    if (!_htmlInitialized) htmlSetCharSet("utf-8");
-    switch (input_type) {
-        case INPUT_TYPE_FILE: switch (data_type) {
-            case DATA_TYPE_TEXT: read_fileurl(TextDatumGetCString(PG_GETARG_DATUM(0)), &document, Path); break;
-            case DATA_TYPE_ARRAY: {
-                if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) E("array_contains_nulls");
-                deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
-                for (int i = 0; i < nelemsp; i++) read_fileurl(TextDatumGetCString(elemsp[i]), &document, Path);
-            } break;
-        } break;
-        case INPUT_TYPE_HTML: switch (data_type) {
-            case DATA_TYPE_TEXT: {
-                text *html = DatumGetTextP(PG_GETARG_DATUM(0));
-                read_html(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html), &document);
-            } break;
-            case DATA_TYPE_ARRAY: {
-                if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) E("array_contains_nulls");
-                deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
-                for (int i = 0; i < nelemsp; i++) {
-                    text *html = DatumGetTextP(elemsp[i]);
-                    read_html(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html), &document);
-                }
-            } break;
-        } break;
-        case INPUT_TYPE_URL: switch (data_type) {
-            case DATA_TYPE_TEXT: read_fileurl(TextDatumGetCString(PG_GETARG_DATUM(0)), &document, NULL); break;
-            case DATA_TYPE_ARRAY: {
-                if (array_contains_nulls(DatumGetArrayTypeP(PG_GETARG_DATUM(0)))) E("array_contains_nulls");
-                deconstruct_array(DatumGetArrayTypeP(PG_GETARG_DATUM(0)), TEXTOID, -1, false, 'i', &elemsp, &nullsp, &nelemsp);
-                for (int i = 0; i < nelemsp; i++) read_fileurl(TextDatumGetCString(elemsp[i]), &document, NULL);
-            } break;
-        } break;
-    }
     while (document && document->prev) document = document->prev;
     htmlFixLinks(document, document, 0);
-    switch (output_type) {
-        case OUTPUT_TYPE_PDF: PSLevel = 0; break;
-        case OUTPUT_TYPE_PS: PSLevel = 3; break;
-    }
     switch (PG_NARGS()) {
-        case 1: if (!(out = open_memstream(&output_data, &output_len))) E("!open_memstream"); break;
+        case 0: if (!(out = open_memstream(&output_data, &output_len))) E("!open_memstream"); break;
         default: {
             char *file;
-            if (PG_ARGISNULL(1)) E("our is null!");
-            file = TextDatumGetCString(PG_GETARG_DATUM(1));
+            if (PG_ARGISNULL(0)) E("file is null!");
+            file = TextDatumGetCString(PG_GETARG_DATUM(0));
             if (!(out = fopen(file, "wb"))) E("!fopen(\"%s\")", file);
             pfree(file);
         } break;
@@ -157,8 +105,9 @@ static Datum htmldoc(PG_FUNCTION_ARGS, data_type_t data_type, input_type_t input
     htmlDeleteTree(document);
     file_cleanup();
     image_flush_cache();
+    document = NULL;
     switch (PG_NARGS()) {
-        case 1: {
+        case 0: {
             text *pdf = cstring_to_text_with_len(output_data, output_len);
             free(output_data);
             PG_RETURN_BYTEA_P(pdf);
@@ -167,17 +116,31 @@ static Datum htmldoc(PG_FUNCTION_ARGS, data_type_t data_type, input_type_t input
     }
 }
 
-EXTENSION(file2pdf) { return htmldoc(fcinfo, DATA_TYPE_TEXT, INPUT_TYPE_FILE, OUTPUT_TYPE_PDF); }
-EXTENSION(file2ps) { return htmldoc(fcinfo, DATA_TYPE_TEXT, INPUT_TYPE_FILE, OUTPUT_TYPE_PS); }
-EXTENSION(file2pdf_array) { return htmldoc(fcinfo, DATA_TYPE_ARRAY, INPUT_TYPE_FILE, OUTPUT_TYPE_PDF); }
-EXTENSION(file2ps_array) { return htmldoc(fcinfo, DATA_TYPE_ARRAY, INPUT_TYPE_FILE, OUTPUT_TYPE_PS); }
+EXTENSION(htmldoc_addfile) {
+    char *file;
+    if (PG_ARGISNULL(0)) E("file is null!");
+    file = TextDatumGetCString(PG_GETARG_DATUM(0));
+    read_fileurl(file);
+    pfree(file);
+    PG_RETURN_BOOL(true);
+}
 
-EXTENSION(html2pdf) { return htmldoc(fcinfo, DATA_TYPE_TEXT, INPUT_TYPE_HTML, OUTPUT_TYPE_PDF); }
-EXTENSION(html2ps) { return htmldoc(fcinfo, DATA_TYPE_TEXT, INPUT_TYPE_HTML, OUTPUT_TYPE_PS); }
-EXTENSION(html2pdf_array) { return htmldoc(fcinfo, DATA_TYPE_ARRAY, INPUT_TYPE_HTML, OUTPUT_TYPE_PDF); }
-EXTENSION(html2ps_array) { return htmldoc(fcinfo, DATA_TYPE_ARRAY, INPUT_TYPE_HTML, OUTPUT_TYPE_PS); }
+EXTENSION(htmldoc_addhtml) {
+    text *html;
+    if (PG_ARGISNULL(0)) E("html is null!");
+    html = DatumGetTextP(PG_GETARG_DATUM(0));
+    read_html(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html));
+    PG_RETURN_BOOL(true);
+}
 
-EXTENSION(url2pdf) { return htmldoc(fcinfo, DATA_TYPE_TEXT, INPUT_TYPE_URL, OUTPUT_TYPE_PDF); }
-EXTENSION(url2ps) { return htmldoc(fcinfo, DATA_TYPE_TEXT, INPUT_TYPE_URL, OUTPUT_TYPE_PS); }
-EXTENSION(url2pdf_array) { return htmldoc(fcinfo, DATA_TYPE_ARRAY, INPUT_TYPE_URL, OUTPUT_TYPE_PDF); }
-EXTENSION(url2ps_array) { return htmldoc(fcinfo, DATA_TYPE_ARRAY, INPUT_TYPE_URL, OUTPUT_TYPE_PS); }
+EXTENSION(htmldoc_addurl) {
+    char *url;
+    if (PG_ARGISNULL(0)) E("url is null!");
+    url = TextDatumGetCString(PG_GETARG_DATUM(0));
+    read_fileurl(url);
+    pfree(url);
+    PG_RETURN_BOOL(true);
+}
+
+EXTENSION(convert2pdf) { PSLevel = 0; return htmldoc(fcinfo); }
+EXTENSION(convert2ps) { PSLevel = 3; return htmldoc(fcinfo); }
