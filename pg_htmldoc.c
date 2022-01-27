cc7850c96ea0053db566ebb272677dc9d7ed50f1
RekGRpth <rekgrpth@gmail.com>
2022-01-27 13:46:48 +0500

    -cleanup

--- pg_htmldoc.c
+++ pg_htmldoc.c
@@ -9,18 +9,23 @@
 
 PG_MODULE_MAGIC;
 
-tree_t *document = NULL;
+#if PG_VERSION_NUM >= 90500
+static struct MemoryContextCallback documentMemoryContextCallback = {0};
+#endif
+static tree_t *document = NULL;
 
 void _PG_init(void); void _PG_init(void) {
     if (!_htmlInitialized) htmlSetCharSet("utf-8");
 }
 
-static void cleanup(void) {
+#if PG_VERSION_NUM >= 90500
+static void documentMemoryContextCallbackFunction(void *arg) {
     if (document) htmlDeleteTree(document);
     file_cleanup();
     image_flush_cache();
     document = NULL;
 }
+#endif
 
 static void read_fileurl(const char *fileurl) {
     tree_t *file;
@@ -28,17 +33,23 @@ static void read_fileurl(const char *fileurl) {
     const char *realname = file_find(Path, fileurl);
     const char *base = file_directory(fileurl);
     _htmlPPI = 72.0f * _htmlBrowserWidth / (PageWidth - PageLeft - PageRight);
-    if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) { cleanup(); ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!htmlAddTree"))); }
+    if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!htmlAddTree")));
     if (!document) document = file; else {
         while (document->next) document = document->next;
         document->next = file;
         file->prev = document;
     }
+#if PG_VERSION_NUM >= 90500
+    if (!documentMemoryContextCallback.func) {
+        documentMemoryContextCallback.func = documentMemoryContextCallbackFunction;
+        MemoryContextRegisterResetCallback(CurrentMemoryContext, &documentMemoryContextCallback);
+    }
+#endif
     htmlSetVariable(file, (uchar *)"_HD_URL", (uchar *)fileurl);
     htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)file_basename(fileurl));
     htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)base);
-    if (!realname) { cleanup(); ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!file_find(\"%s\", \"%s\")", Path, fileurl))); }
-    if (!(in = fopen(realname, "rb"))) { cleanup(); ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!fopen(\"%s\")", realname))); }
+    if (!realname) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!file_find(\"%s\", \"%s\")", Path, fileurl)));
+    if (!(in = fopen(realname, "rb"))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!fopen(\"%s\")", realname)));
     htmlReadFile2(file, in, base);
     fclose(in);
 }
@@ -47,15 +58,21 @@ static void read_html(char *html, size_t len) {
     tree_t *file;
     FILE *in;
     _htmlPPI = 72.0f * _htmlBrowserWidth / (PageWidth - PageLeft - PageRight);
-    if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) { cleanup(); ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!htmlAddTree"))); }
+    if (!(file = htmlAddTree(NULL, MARKUP_FILE, NULL))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!htmlAddTree")));
     if (!document) document = file; else {
         while (document->next) document = document->next;
         document->next = file;
         file->prev = document;
     }
+#if PG_VERSION_NUM >= 90500
+    if (!documentMemoryContextCallback.func) {
+        documentMemoryContextCallback.func = documentMemoryContextCallbackFunction;
+        MemoryContextRegisterResetCallback(CurrentMemoryContext, &documentMemoryContextCallback);
+    }
+#endif
     htmlSetVariable(file, (uchar *)"_HD_FILENAME", (uchar *)"html");
     htmlSetVariable(file, (uchar *)"_HD_BASE", (uchar *)".");
-    if (!(in = fmemopen(html, len, "rb"))) { cleanup(); ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!fmemopen"))); }
+    if (!(in = fmemopen(html, len, "rb"))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!fmemopen")));
     htmlReadFile2(file, in, ".");
     fclose(in);
 }
@@ -64,20 +81,20 @@ static Datum htmldoc(PG_FUNCTION_ARGS) {
     char *output_data = NULL;
     size_t output_len = 0;
     FILE *out;
+    if (!document) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!document")));
     while (document && document->prev) document = document->prev;
     htmlFixLinks(document, document, 0);
     switch (PG_NARGS()) {
-        case 0: if (!(out = open_memstream(&output_data, &output_len))) { cleanup(); ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!open_memstream"))); } break;
+        case 0: if (!(out = open_memstream(&output_data, &output_len))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!open_memstream"))); break;
         default: {
             char *file;
-            if (PG_ARGISNULL(0)) { cleanup(); ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc requires argument file"))); }
+            if (PG_ARGISNULL(0)) ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc requires argument file")));
             file = TextDatumGetCString(PG_GETARG_DATUM(0));
-            if (!(out = fopen(file, "wb"))) { cleanup(); ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!fopen(\"%s\")", file))); }
+            if (!(out = fopen(file, "wb"))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("!fopen(\"%s\")", file)));
             pfree(file);
         } break;
     }
     pspdf_export_out(document, NULL, out);
-    cleanup();
     switch (PG_NARGS()) {
         case 0: {
             text *pdf = cstring_to_text_with_len(output_data, output_len);
@@ -90,7 +107,7 @@ static Datum htmldoc(PG_FUNCTION_ARGS) {
 
 EXTENSION(htmldoc_addfile) {
     char *file;
-    if (PG_ARGISNULL(0)) { cleanup(); ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc_addfile requires argument file"))); }
+    if (PG_ARGISNULL(0)) ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc_addfile requires argument file")));
     file = TextDatumGetCString(PG_GETARG_DATUM(0));
     read_fileurl(file);
     pfree(file);
@@ -99,7 +116,7 @@ EXTENSION(htmldoc_addfile) {
 
 EXTENSION(htmldoc_addhtml) {
     text *html;
-    if (PG_ARGISNULL(0)) { cleanup(); ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc_addhtml requires argument html"))); }
+    if (PG_ARGISNULL(0)) ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc_addhtml requires argument html")));
     html = DatumGetTextP(PG_GETARG_DATUM(0));
     read_html(VARDATA_ANY(html), VARSIZE_ANY_EXHDR(html));
     PG_RETURN_BOOL(true);
@@ -107,7 +124,7 @@ EXTENSION(htmldoc_addhtml) {
 
 EXTENSION(htmldoc_addurl) {
     char *url;
-    if (PG_ARGISNULL(0)) { cleanup(); ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc_addurl requires argument url"))); }
+    if (PG_ARGISNULL(0)) ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("htmldoc_addurl requires argument url")));
     url = TextDatumGetCString(PG_GETARG_DATUM(0));
     read_fileurl(url);
     pfree(url);
