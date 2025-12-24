baba2fbe98dbb54fec7d5f81ee5c89f5635874c9
Georgy Shelkovy <rekgrpth@gmail.com>
2025-12-24 21:22:35 +0500

    rename

--- html.h
+++ html.h
@@ -152,7 +152,7 @@ extern float		_htmlPPI;
 extern float		_htmlBrowserWidth;
 extern int		_htmlInitialized;
 
-tree_t	*htmlReadFile2(tree_t *parent, FILE *fp, const char *base);
+tree_t	*htmlReadFile(tree_t *parent, FILE *fp, const char *base);
 tree_t	*htmlAddTree(tree_t *parent, markup_t markup, uchar *data);
 int	htmlDeleteTree(tree_t *parent);
 void	htmlFixLinks(tree_t *doc, tree_t *tree, uchar *base);
